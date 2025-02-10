aab30af1e5a775ec0c5187765319e4c712b7c4fd
NandaDwiCahyo <nandad.cahyo@gmail.com>
2025-02-10 23:32:04 +0700

    Create Button for HumanScene

---
+++ Assets/Scripts/Human.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Human : MonoBehaviour
+{
+    public void AllBody()
+    {
+        SceneManager.LoadSceneAsync("AllBody");
+    }
+}
